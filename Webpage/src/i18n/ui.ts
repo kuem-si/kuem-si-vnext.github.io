afa2072323188b186afa74aa50d71b8f8027c5f3
Leon Košak <leon.kosak@gmail.com>
2024-03-26 21:03:05 +0100

    multilanguage support added

---
+++ Webpage/src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: 'English',
+    sl: 'Slovensko',
+  };
+  
+  export const defaultLang = 'sl';
+
+  export const showDefaultLang = false;
+  
+  export const ui = {
+    en: {
+      'nav.contact': 'Contact',
+      'nav.solutions': 'Solutions',
+      'nav.services': 'Services',
+      'nav.about': 'About',
+      'nav.language': 'Language',
+    },
+    sl: {
+      'nav.contact': 'Kontakt',
+      'nav.solutions': 'Rešitve',
+      'nav.services': 'Storitve',
+      'nav.about': 'O nas',
+      'nav.language': 'Jezik',
+    },
+  } as const;

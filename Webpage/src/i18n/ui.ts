f9c867206ceb0e9dbec993800936281e5326c731
Leon Košak <leon.kosak@gmail.com>
2024-03-29 09:46:36 +0100

    menu 18n, custom routes

--- Webpage/src/i18n/ui.ts
+++ Webpage/src/i18n/ui.ts
@@ -6,20 +6,39 @@ export const languages = {
   export const defaultLang = 'sl';
 
   export const showDefaultLang = false;
+
+  export const routes = {
+    en: {
+      'resitvenexavia': 'solutions/nexavia',
+      'resitvethynkr': 'solutions/thynkr',
+      'storitverazvoj-programske-opreme': 'services/software-development',
+      'storitvearhitektura-programske-opreme-in-svetovanje': 'services/software-arhitecture-and-consulting',
+      'storitvedevops-in-platform-engineering': 'services/devops-and-platform-engineering',
+      'o-nas': 'about',
+      'kontakt': 'contact',
+    }
+  }
   
   export const ui = {
     en: {
       'nav.contact': 'Contact',
       'nav.solutions': 'Solutions',
-      'nav.services': 'Services',
+      'nav.services': 'Additional Services',
       'nav.about': 'About',
-      'nav.language': 'Language',
+      'nav.softwaredevelopment': 'Software Development',
+      'nav.softwarearhitectureandconsulting': 'Software Arhitecture and Consulting',
+      'nav.devopsandplatformengineering': 'DevOps and Platform Engineering',
     },
     sl: {
       'nav.contact': 'Kontakt',
       'nav.solutions': 'Rešitve',
-      'nav.services': 'Storitve',
+      'nav.services': 'Dodatne storitve',
       'nav.about': 'O nas',
-      'nav.language': 'Jezik',
+      'nav.softwaredevelopment': 'Razvoj programske opreme',
+      'nav.softwarearhitectureandconsulting': 'Arhitektura programske opreme in svetovanje',
+      'nav.devopsandplatformengineering': 'DevOps in Platform Engineering',
+
+      'nexavia': 'Nexavia',
+      'thynkr': 'Thynkr',
     },
   } as const;

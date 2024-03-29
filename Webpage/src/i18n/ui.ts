45aa8d7b8d30c5b3c0eaa0fdaf3141c5641e3c79
Leon Košak <leon.kosak@gmail.com>
2024-03-29 20:54:31 +0100

    dev landing page

--- Webpage/src/i18n/ui.ts
+++ Webpage/src/i18n/ui.ts
@@ -35,6 +35,16 @@ export const languages = {
       'landingpage:maincompanytitledescription': 'Tailored software solutions and consulting services designed to meet the most demanding challenges.',
       'landingpage:maincompanytitledescriptioninformation': 'KUEM brings together experts with extensive experience in various complex software solutions.',
       'landingpage:trustedby': 'Trusted by',
+      'landingpage:solutionsandservicesoffer': 'Solutions and Servicess offer',
+      'landingpage:solutionsandservicesofferdescription': 'Providing comprehensive solutions in one place',
+      'landingpage:solutionsandservicesofferapplicationfamily': 'Family of Applications',
+      'landingpage:solutionsandservicesofferapplicationfamilydescription': 'Modern multipurpose IoT platforms designed for monitoring smart devices across various LPWAN (Low-Power WAN) networks.',
+      'landingpage:solutionsandservicesoffersoftwaredevelopment': 'Software Development and Custom System Integrations',
+      'landingpage:solutionsandservicesoffersoftwaredevelopmentdescription': 'Maximum optimizations when digitalizing business processes can be achived with custom developed software solutions.',
+      'landingpage:solutionsandservicesoffersoftwarearhitectureandconsulting': 'Arhitecture and Consulting',
+      'landingpage:solutionsandservicesoffersoftwarearhitectureandconsultingdescription': 'A well-designed software solution results in fewer scalability issues in the future.',
+      'landingpage:solutionsandservicesofferdevopsandplatformengineering': 'DevOps & Platform Engineering',
+      'landingpage:solutionsandservicesofferdevopsandplatformengineeringdescription': 'We assist you in establishing the entire modern container-based execution environment with all configuration and applications.',
 
       'footer:allrightsreserved': 'All rights reserved',
       'footer:socialnetworks': 'Social Networks',
@@ -42,6 +52,7 @@ export const languages = {
     sl: {
       'kuem': 'Kuem',
       'kuemdoo': 'Kuem d.o.o.',
+      'linkedin': 'LinkedIn',
 
       'global:moreinfo': 'Več informacij',
 
@@ -57,6 +68,16 @@ export const languages = {
       'landingpage:maincompanytitledescription': 'Dovršene programske rešitve in svetovanja primerna za najzahtevnajše izzive.',
       'landingpage:maincompanytitledescriptioninformation': 'KUEM združuje strokovnjake z bogatimi izkušnjami na področju najrazličnejših kompleksnih programskih rešitev.',
       'landingpage:trustedby': 'Zaupajo nam',
+      'landingpage:solutionsandservicesoffer': 'Ponudba rešitev in storitev',
+      'landingpage:solutionsandservicesofferdescription': 'Zagotavljanje celostnih rešitev na enem mestu',
+      'landingpage:solutionsandservicesofferapplicationfamily': 'Družina aplikacij',
+      'landingpage:solutionsandservicesofferapplicationfamilydescription': 'Moderne splošnonamenske IoT platforme namenjene nadzoru pametnih naprav preko različnih LPWAN (Low-Power WAN) omrežij.',
+      'landingpage:solutionsandservicesoffersoftwaredevelopment': 'Razvoj programske opreme in integracij na ključ',
+      'landingpage:solutionsandservicesoffersoftwaredevelopmentdescription': 'Največje optimizacije pri digitalizaciji poslovnih procesov in izmenjavah podatkov dosegamo z namensko razvito programsko opremo.',
+      'landingpage:solutionsandservicesoffersoftwarearhitectureandconsulting': 'Arhitektura in svetovanje',
+      'landingpage:solutionsandservicesoffersoftwarearhitectureandconsultingdescription': 'Dobro zasnovana programska rešitev pomeni manj težav s skalabilnostjo v prihodnosti.',
+      'landingpage:solutionsandservicesofferdevopsandplatformengineering': 'DevOps & Platform Engineering',
+      'landingpage:solutionsandservicesofferdevopsandplatformengineeringdescription': 'Pomagamo vam vzpostaviti celotno moderno kontejnersko izvajalno okolje z vso potrebno konfiguracijo in aplikacijami.',
 
       'footer:allrightsreserved': 'Vse pravice pridržane',
       'footer:socialnetworks': 'Socialna omrežja',
